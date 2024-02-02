417a15a4fb118aad1ff6486d071fc4d3e1c9ad20
shohanshovo11 <101856535+shohanshovo11@users.noreply.github.com>
2024-02-02 17:22:59 +0600

    book add api added

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const Book = require("./model/bookModel");
 const app = express();
 const uri =
-  "mongodb+srv://waterhorse08:123@hackathon.i8cmqm5.mongodb.net/?retryWrites=true&w=majority";
+  "mongodb+srv://waterhorse08:123@hackathon.i8cmqm5.mongodb.net/hackathon";
 mongoose.connect(uri);
-
+app.use(express.json());
 app.listen(5000, console.log("Server is running on port 5000"));
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
-app.post("/api/books", (req, res) => {});
+app.post("/api/books", async (req, res) => {
+  try {
+    console.log(req.body);
+    const newBook = new Book(req.body);
+    const savedBook = await newBook.save();
+    res.status(201).json(savedBook);
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
