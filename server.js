0f675a473900aaf7a38392394cfd9a2889dae99e
shohanshovo11 <101856535+shohanshovo11@users.noreply.github.com>
2024-02-02 18:17:58 +0600

    final

--- server.js
+++ server.js
@@ -20,3 +20,67 @@ app.post("/api/books", async (req, res) => {
     res.status(500).json({ error: "Internal Server Error" });
   }
 });
+app.put("/api/books/:id", async (req, res) => {
+  const bookId = req.params.id;
+  try {
+    const existingBook = await Book.findOne({ id: bookId });
+    console.log(existingBook);
+    if (!existingBook) {
+      return res.status(404).json({ error: "Book not found" });
+    }
+    existingBook.title = req.body.title;
+    existingBook.author = req.body.author;
+    existingBook.genre = req.body.genre;
+    existingBook.price = req.body.price;
+
+    const updatedBook = await existingBook.save();
+    res.status(200).json(updatedBook);
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+app.get("/api/books", async (req, res) => {
+  const { title, author, genre, sort, order } = req.query;
+  const sortOptions = {};
+  if (sort) {
+    sortOptions[sort] = order === "DESC" ? -1 : 1;
+  } else {
+    sortOptions.id = 1;
+  }
+  try {
+    const query = {};
+    if (title) query.title = title;
+    if (author) query.author = author;
+    if (genre) query.genre = genre;
+    console.log(query);
+    const foundBooks = await Book.find(query).sort(sortOptions);
+    res.status(200).json({ books: foundBooks });
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+app.get("/api/books/:id", async (req, res) => {
+  try {
+    const bookId = req.params.id;
+    const book = await Book.find({ id: bookId });
+    console.log(book);
+
+    if (book.length > 0) {
+      res.status(200).json(book);
+    } else {
+      res
+        .status(404)
+        .json({ message: `Book with id: ${bookId} was not found` });
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+app.get("/api/books", async (req, res) => {
+  try {
+    const allBooks = await Book.find().sort({ id: 1 });
+    res.status(200).json({ books: allBooks });
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
